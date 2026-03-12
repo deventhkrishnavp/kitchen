c80fafe334a6986ab49460e536e44bd0f0b0ad5c
deventhkrishnavp <deventhkrishnavp595@gmail.com>
2026-03-12 18:12:23 +0530

    update allowed hosts

--- cafe_project/settings.py
+++ cafe_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-r5^6((!h(@t8c#f19t@nozurf3rml*hrhjlfm(*j1c7icgv8+!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kitchen-2-ojkz.onrender.com']
+ALLOWED_HOSTS = [
+    "kitchen-2-ojkz.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
